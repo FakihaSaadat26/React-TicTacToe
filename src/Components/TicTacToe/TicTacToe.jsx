7a53e3fcb0a76f5d0915ad3209090822bb55443b
FakihaSaadat26 <fakiha.saadat@gmail.com>
2025-05-06 23:25:28 +0500

    board setup and styling

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,112 @@
+// import React, { useState } from 'react'
+// import './TicTacToe.css'
+// import circle_icon from '../Assets/download.png'
+// import cross_icon from '../Assets/download.jpg'
+
+// let data = ["","","","","","","","","",""];
+// const TicTacToe = () => {
+//     let [count, setCount] = useState(0);
+//     let [lock, setLock] = useState(false);
+    
+//     const toggle = (w,num) =>{
+//         if (lock) {
+//             return 0;
+//         }
+//         if (count%2===0){
+//             e.target.innerHTML = `<img src'${cross_icon}'>`;
+//             data[num] = x;
+//             setCount(count+1);
+//         }
+//         else{
+//             e.target.innerHTML = `<img src'${circle_icon}'>`;
+//             data[num] = o;
+//             setCount(count+1);
+//         }
+
+//     }
+
+//     return (
+//         <div className='container'>
+//             <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
+//             <div className="board">
+//                 <div className="row1">
+//                     <div className="boxes" onClick={(e) => {toggle(e,0)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,1)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,2)}}></div>
+//                 </div>
+//                 <div className="row2">
+//                     <div className="boxes" onClick={(e) => {toggle(e,3)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,4)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,5)}}></div>
+//                 </div>
+//                 <div className="row3">
+//                     <div className="boxes" onClick={(e) => {toggle(e,6)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,7)}}></div>
+//                     <div className="boxes" onClick={(e) => {toggle(e,8)}}></div>
+//                 </div>
+//             </div>
+//             <button className='reset'>Reset</button>
+//         </div>
+//     )
+// }
+
+// export default TicTacToe
+
+import React, { useState } from 'react';
+import './TicTacToe.css';
+import circle_icon from '../Assets/download.png';
+import cross_icon from '../Assets/download.jpg';
+
+const TicTacToe = () => {
+  const [data, setData] = useState(Array(9).fill(""));
+  const [count, setCount] = useState(0);
+  const [lock, setLock] = useState(false);
+
+  const toggle = (index) => {
+    if (lock || data[index] !== "") return;
+
+    const newData = [...data];
+    newData[index] = count % 2 === 0 ? "x" : "o";
+    setData(newData);
+    setCount(count + 1);
+  };
+
+  const resetGame = () => {
+    setData(Array(9).fill(""));
+    setCount(0);
+    setLock(false);
+  };
+
+  const getIcon = (value) => {
+    if (value === "x") return <img src={cross_icon} alt="X" />;
+    if (value === "o") return <img src={circle_icon} alt="O" />;
+    return null;
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        Tic Tac Toe Game In <span>React</span>
+      </h1>
+      <div className="board">
+        {[0, 1, 2].map((row) => (
+          <div className={`row${row + 1}`} key={row}>
+            {[0, 1, 2].map((col) => {
+              const index = row * 3 + col;
+              return (
+                <div className="boxes" key={index} onClick={() => toggle(index)}>
+                  {getIcon(data[index])}
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+      <button className="reset" onClick={resetGame}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default TicTacToe;
